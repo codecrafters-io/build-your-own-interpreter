141e67fc1e0898131a21a503b08ba91aabf5c71c
Ryan Gang <ryan-gg@outlook.com>
2024-07-01 16:29:14 +0530

    feat: update go starter template

---
+++ solutions/go/01-dg6/code/cmd/myinterpreter/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Error: Missing file argument")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting file info: %v\n", err)
+		os.Exit(1)
+	}
+
+	fileContents := make([]byte, fileInfo.Size())
+	_, err = file.Read(fileContents)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(fileContents) > 0 {
+		panic("Scanner not implemented")
+	} else {
+		fmt.Println("EOF  NULL") // Placeholder, remove this line when implementing the scanner
+	}
+}
---
+++ solutions/go/01-dg6/explanation.md
@@ -0,0 +1,20 @@
+The entry point for your Interpreter implementation is in `cmd/myinterpreter/main.go`.
+
+Study and uncomment the relevant code: 
+
+```go
+// Uncomment this block to pass the first stage
+if len(fileContents) > 0 {
+	panic("Scanner not implemented")
+} else {
+	fmt.Println("EOF  NULL") // Placeholder, remove this line when implementing the scanner
+}
+```
+
+Push your changes to pass the first stage:
+
+```
+git add .
+git commit -m "pass 1st stage" # any msg
+git push origin master
+```
