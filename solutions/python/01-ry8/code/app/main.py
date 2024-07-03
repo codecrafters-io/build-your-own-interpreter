82d800ba230a8320281f1ba471f3239f1e9e2ae5
Ryan Gang <ryan-gg@outlook.com>
2024-07-04 02:55:42 +0530

    feat: Update file open argument in Python code
    The code changes modify the file open argument in the Python code. The previous code used `sys.argv[1]` to open a file, but it has been updated to use `sys.argv[2]` instead. This change ensures that the correct file is opened and read.

--- solutions/python/01-ry8/code/app/main.py
+++ solutions/python/01-ry8/code/app/main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    with open(sys.argv[1]) as file:
+    with open(sys.argv[2]) as file:
         file_contents = file.read()
 
     if file_contents:
