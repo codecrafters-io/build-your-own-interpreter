99b2e0eb228b2f550360d37170c27a6e013733eb
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-05 16:20:29 +0100

    Refactor Rust starter templates and Dockerfile for CodeCrafters

--- solutions/rust/01-ry8/code/your_program.sh
+++ solutions/rust/01-ry8/code/your_program.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 #
-# DON'T EDIT THIS!
+# Use this script to run your program LOCALLY.
 #
-# CodeCrafters uses this file to test your code. Don't make any changes here!
+# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
 #
-# DON'T EDIT THIS!
-exec cargo run \
+# Learn more: https://codecrafters.io/program-interface
+
+# Exit early if any commands fail
+set -e
+
+# Copied from .codecrafters/compile.sh
+#
+# - Edit this to change how your program compiles locally
+# - Edit .codecrafters/compile.sh to change how your program compiles remotely
+cargo build \
     --quiet \
     --release \
     --target-dir=/tmp/codecrafters-interpreter-target \
-    --manifest-path $(dirname $0)/Cargo.toml -- "$@"
+    --manifest-path Cargo.toml
+
+# Copied from .codecrafters/run.sh
+#
+# - Edit this to change how your program runs locally
+# - Edit .codecrafters/run.sh to change how your program runs remotely
+exec /tmp/codecrafters-interpreter-target/release/interpreter-starter-rust "$@"
