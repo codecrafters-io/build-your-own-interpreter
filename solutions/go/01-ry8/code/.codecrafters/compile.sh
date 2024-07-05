dc0f0cfa806e71545109a8c7ac06df99f1b3f91e
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-05 16:09:59 +0100

    Revert "Use new interface for Go starter"

--- solutions/go/01-ry8/code/.codecrafters/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# This script is used to compile your program on CodeCrafters
-#
-# This runs before .codecrafters/run.sh
-#
-# Learn more: https://codecrafters.io/program-interface
-
-# (This file is empty since Go programs don't use a compile step)
--- starter_templates/go/.codecrafters/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# This script is used to run your program on CodeCrafters
-#
-# This runs after .codecrafters/compile.sh
-#
-# Learn more: https://codecrafters.io/program-interface
-
