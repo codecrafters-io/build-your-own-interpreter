316a7437a44a585848d7488fde98a7054218762f
Alexey Ismagilov <al.a.ismagilov@gmail.com>
2024-07-13 01:52:38 +0300

    Add CPP starter template

---
+++ starter_templates/cpp/code/src/main.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  std::cerr << "Logs from your program will appear here!" << std::endl;
+
+  if (argc < 3) {
+    std::cerr << "Usage: ./your_program.sh tokenize <filename>" << std::endl;
+    return 1;
+  }
+
+  std::string command{argv[1]};
+  std::string filename{argv[2]};
+
+  if (command != "tokenize") {
+    std::cerr << "Unknown command: " << command << std::endl;
+    return 1;
+  }
+
+  std::ifstream file{filename};
+  using Iterator = std::istreambuf_iterator<char>;
+  std::string file_contents{Iterator{file}, Iterator{}};
+
+  // Uncomment this block to pass the first stage
+  // if (!file_contents.empty()) {
+  //   std::cerr << "Scanner not implemented" << std::endl;
+  // } else {
+  //   std::cout << "EOF  null" << std::endl; // Placeholder, remove this line when implementing the scanner
+  // }
+}
