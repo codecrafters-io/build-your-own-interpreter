f43a23eb698e52e7d46a24a66220a995730fd18b
Ryan Gang <ryan-gg@outlook.com>
2024-07-26 01:52:08 +0530

    chore: run course-sdk compile

--- compiled_starters/go/your_program.sh
+++ compiled_starters/go/your_program.sh
@@ -12,7 +12,10 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
-go build -o /tmp/interpreter-target ./cmd/myinterpreter
+(
+  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+  go build -o /tmp/interpreter-target ./cmd/myinterpreter
+)
 
 # Copied from .codecrafters/run.sh
 #
