188fbac447743f1be4171d15925217678c6d15db
Ryan Gang <ryan-gg@outlook.com>
2024-11-20 10:19:00 +0530

    feat: add compiled files for starter

---
+++ solutions/csharp/01-ry8/code/src/main.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+
+if (args.Length < 2)
+{
+    Console.Error.WriteLine("Usage: ./your_program.sh tokenize <filename>");
+    Environment.Exit(1);
+}
+
+string command = args[0];
+string filename = args[1];
+
+if (command != "tokenize")
+{
+    Console.Error.WriteLine($"Unknown command: {command}");
+    Environment.Exit(1);
+}
+
+string fileContents = File.ReadAllText(filename);
+
+if (!string.IsNullOrEmpty(fileContents))
+{
+    throw new NotImplementedException("Scanner not implemented");
+}
+else
+{
+    Console.WriteLine("EOF  null"); // Placeholder, remove this line when implementing the scanner
+}
