ef0db2a74cdaf0d3808d14bcc27592dfe655db1f
nas <nas@denk-solutions.com>
2025-08-31 12:03:08 +0200

    make sure build script does not use deprecated .root_source_file property

--- compiled_starters/zig/build.zig
+++ compiled_starters/zig/build.zig
@@ -4,9 +4,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "main",
-        .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.standardTargetOptions(.{}),
+            .optimize = b.standardOptimizeOption(.{}),
+        }),
     });
 
     // This declares intent for the executable to be installed into the
